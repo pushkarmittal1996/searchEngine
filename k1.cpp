e92cb3b6fc3b99e50eaaac51194a0273e45c01d0
root <mittal.pushkar@gmail.com>
2018-01-19 19:46:05 +0000

    adapter for docker on windows

--- k1.cpp
+++ k1.cpp
@@ -1,5 +1,5 @@
 // mach1.cpp : Defines the entry point for the console application.
-//
+
 #include <map>
 #include "trie.cpp"
 using namespace std;
@@ -16,22 +16,25 @@ void simulate1()//3:06 min 3.371GB
 	trie<double> t;
 	int i;
 	int y;
-	for (i = 1; i < 10000000; i++)t.insert(toString(i), i);
-	for (i = 1; i < 10000000; i++)t.search(toString(i));
-	for (i = 1; i < 10000000; i++)t.remove(toString(i));
+	for (i = 1; i < 1000000; i++)t.insert(toString(i), i);
+	for (i = 1; i < 1000000; i++)t.search(toString(i));
+	for (i = 1; i < 1000000; i++)t.remove(toString(i));
 
 }
 void simulate2()//3:48 min 3.829GB
 {
 	int i;
 	map<string, double>t;
-	for (i = 1; i < 10000000; i++)t[toString(i)] = i;
-	for (i = 1; i < 10000000; i++)t[toString(i)];
-	for (i = 1; i < 10000000; i++)t.erase(toString(i));
+	for (i = 1; i < 1000000; i++)t[toString(i)] = i;
+	for (i = 1; i < 1000000; i++)t[toString(i)];
+//	for (i = 1; i < 1000000; i++)t.erase(toString(i));
 }
 int main()
 {
-	//simulate1();
-	simulate2();
-	return 0;
+	simulate1();
+	//simulate2();
+	cout<<"finished\n";
+	int y;
+	cin>>y;
+	//return 0;
 }
